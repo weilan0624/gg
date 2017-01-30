e24fb75ec0eb20216de911834b47cbf464cdb4f8
Wei lan <Wei lan@DESKTOP-TAQ4E0R>
2017-01-30 14:46:15 +0000

    fd

---
+++ setup.py
@@ -0,0 +1,13 @@
+from  setuptools import setup
+
+setup(name="systeminfo",
+      version="0.1",
+      url="",
+      author="Wei lan",
+      author_email="lan.wei@ucdconnect.ie",
+      licence="GPL3",
+      packages=['systeminfo'],
+      entry_points={
+          'console_scripts':['comp30670_systeminfo=systeminfo.main:main']
+          }
+      )
